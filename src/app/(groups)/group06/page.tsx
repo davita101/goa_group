5f7ed4e7ae85dd48c37760e12f8e9cc4be407af1
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2024-11-05 23:41:52 +0400

    11/05/2024/

--- src/app/(groups)/group06/page.tsx
+++ src/app/(groups)/group06/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import TableComponent from "@/src/components/TableComponent"
 
+const group = [{"name":"თემო ველიჯანაშვილი","score":60},
+  {"name":"შიო ელიკაშვილი","score":35},
+  {"name":"ლუკა გიორგაძე","score":25},
+  {"name":"ანდრია ღადუაშვილი","score":20},
+  {"name":"დათო მელიქიძე","score":5},
+  {"name":"ბექა ბეჟანიშვილი","score":5},
+  {"name":"ბუბა ებრალიძე","score":0},
+  {"name":"ლაზაღე ღოღობერიძე","score":0},
+  {"name":"დავით მეფარიშვილი","score":0},
+  {"name":"ნოდიკო ბეჟანიძე","score":0},
+  {"name":"ნიკოლოზ ჩაგანავა","score":0},
+  {"name":"დაჩი ჯაჯანაშვილი","score":0},
+  {"name":"ლაზრე ღოღობერიძე","score":0}]
 export default function page() {
   return (
     <div>
-      group6
+      <TableComponent arr={group} urlId="06" />
     </div>
   )
 }
+
--- src/app/(groups)/group38/page.tsx
+++ src/app/(groups)/group38/page.tsx
@@ -2,20 +2,7 @@ import TableComponent from '@/src/components/TableComponent'
 import React from 'react'
 
 export default function Page() {
-    const group =  [
-        { "name": "ლუკა რაზმაძე", "score": 0 },
-        { "name": "ანდრია ჯანეზაშვილი", "score": 0 },
-        { "name": "კესანე ბრეგვაძე", "score": 0 },
-        { "name": "კონსტანტინე პაპავა", "score": 0 },
-        { "name": "დავიდ დუმბაძე", "score": 0 },
-        { "name": "დემეტრე არეშიძე", "score": 0 },
-        { "name": "ნიკოლოზ ტატიუაშვილი", "score": 0 },
-        { "name": "გუგა აკოლაშვილი", "score": 0 },
-        { "name": "ლუკა დათიაშვილი", "score": 0 },
-        { "name": "აკაკი წერეთელი", "score": 0 },
-        { "name": "გიორგი წიკლაური", "score": 0 },
-        { "name": "მათე კუკუტარია", "score": 0 }
-    ]
+    const group =  [{"name":"კესანე ბრეგვაძე","score":85},{"name":"ლუკა დათიაშვილი","score":75},{"name":"ლუკა ოშორიზე","score":50},{"name":"ლუკა რაზმაძე","score":50},{"name":"დავიდ დუმბაძე","score":20},{"name":"ანდრია დვალი","score":35},{"name":"ანდრია ჯანეზაშვილი","score":0},{"name":"კონსტანტინე პაპავა","score":0},{"name":"დემეტრე არეშიძე","score":0},{"name":"ნიკოლოზ ტატიუაშვილი","score":0},{"name":"გუგა აკოლაშვილი","score":0},{"name":"აკაკი წერეთელი","score":0},{"name":"გიორგი წიკლაური","score":0},{"name":"მათე კუკუტარია","score":0}]
   return (
     <div>
       <TableComponent arr={group} urlId='38'/>
