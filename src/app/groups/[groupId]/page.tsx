9328a0c7728ff14841f56407ae87249d5b8e1161
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2024-11-15 18:51:39 +0400

    fixDB

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -1,11 +1,18 @@
 "use client"
 import TableComponent from "@/src/components/TableComponent"
 import { groupObject } from "../../constants/groupObject"
- 
+
 export default function GroupDetails({ params }: { params: { groupId: string } }) {
+  const groupData = groupObject[Number(params.groupId)];
+
+  const normalizedGroupData = groupData?.map(item => ({
+    ...item,
+    score: typeof item.score === 'string' ? parseFloat(item.score) : item.score,
+  })) || [];
+
   return (
     <div>
-      <TableComponent arr={groupObject[Number(params.groupId)]} urlId={params.groupId} />
+      <TableComponent arr={normalizedGroupData} urlId={params.groupId} />
     </div>
-  )
+  );
 }
