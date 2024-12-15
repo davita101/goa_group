644e88e3384a9da454e888f865d1e39465a29d0c
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2024-12-15 17:28:54 +0400

    ...

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -2,14 +2,33 @@
 import TableComponent from "@/src/components/TableComponent"
 import { groupObject } from "../../constants/groupObject"
 
-export default function GroupDetails({ params }: { params: { groupId: string } }) {
-  const groupData = groupObject[Number(params.groupId)];
+interface Student {
+  name: string;
+  studentAttend: number;
+  help: number;
+  cameraOn: number;
+  answers: number;
+  score: number;
+  classwork: number;
+  extraProjects: number;
+}
 
-  const normalizedGroupData = groupData?.map(item => (
-    {...item,score: typeof item.score === 'string' ? parseFloat(item.score) : item.score,})) || [];
+export default function GroupDetails({ params }: { params: { groupId: string } }) {
+  const groupData: (Student | { name: string; score: number; classwork: number; extraProjects: number; })[] = groupObject[Number(params.groupId)];
+
+  const normalizedGroupData: Student[] = groupData?.map(item => ({
+    name: item.name,
+    score: typeof item.score === 'string' ? parseFloat(item.score) : item.score,
+    studentAttend: 'studentAttend' in item ? item.studentAttend : 0,
+    help: 'help' in item ? item.help : 0,
+    cameraOn: 'cameraOn' in item ? item.cameraOn : 0,
+    answers: 'answers' in item ? item.answers : 0,
+    classwork: item.classwork,
+    extraProjects: item.extraProjects,
+  })) || [];
 
   return (
-    <div >
+    <div>
       <TableComponent arr={normalizedGroupData} urlId={params.groupId} />
     </div>
   );
